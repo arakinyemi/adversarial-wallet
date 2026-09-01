48a48fe1946155b86228c4501d953b401afb08a9
Araoluwa Akinyemi <arakinyemi@gmail.com>
2026-09-01 02:05:55 +0100

    Apply the Signal design system: Space Grotesk + IBM Plex Mono, lime-on-black palette

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import "@fontsource/space-grotesk/400.css";
+import "@fontsource/space-grotesk/500.css";
+import "@fontsource/space-grotesk/600.css";
+import "@fontsource/space-grotesk/700.css";
+import "@fontsource/ibm-plex-mono/400.css";
+import "@fontsource/ibm-plex-mono/500.css";
+import "@fontsource/ibm-plex-mono/600.css";
 import "./ui/theme.css";
 
 createRoot(document.getElementById("root")!).render(
